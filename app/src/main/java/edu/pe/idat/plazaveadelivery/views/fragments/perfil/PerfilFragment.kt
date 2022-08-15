50b00474c765af5592dd549a42564f825602afcc
Andrea Sotil <andreasotil@ieholding.com>
2022-08-14 22:06:27 -0500

    mi perfil

--- app/src/main/java/edu/pe/idat/plazaveadelivery/views/fragments/perfil/PerfilFragment.kt
+++ app/src/main/java/edu/pe/idat/plazaveadelivery/views/fragments/perfil/PerfilFragment.kt
@@ -1,16 +1,22 @@
 package edu.pe.idat.plazaveadelivery.views.fragments.perfil
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
+import edu.pe.idat.plazaveadelivery.R
 import edu.pe.idat.plazaveadelivery.databinding.FragmentPerfilBinding
 import edu.pe.idat.plazaveadelivery.db.entity.UsuarioEntity
+import edu.pe.idat.plazaveadelivery.utils.SharedPrefCons
 import edu.pe.idat.plazaveadelivery.viewmodel.UsuarioRoomViewModel
+import edu.pe.idat.plazaveadelivery.views.LoginActivity
+import edu.pe.idat.plazaveadelivery.views.MainActivity
 
-class PerfilFragment : Fragment() {
+class PerfilFragment : Fragment(){
 
     private var _binding: FragmentPerfilBinding? = null
     private val binding get() = _binding!!
@@ -26,9 +32,12 @@ class PerfilFragment : Fragment() {
     ): View {
         _binding = FragmentPerfilBinding.inflate(inflater, container, false)
 
+
         usuarioRoomViewModel = ViewModelProvider(requireActivity())[UsuarioRoomViewModel::class.java]
 
         getUserFromDB()
+
+        binding.btnlogout.setOnClickListener{logout()}
 
         return binding.root
     }
@@ -43,7 +52,31 @@ class PerfilFragment : Fragment() {
     }
 
     private fun cargarDatos() {
+        binding.tvNombreRepartidor.text = usuario.nombre
+        binding.tvApellidoRepartidor.text = usuario.apellidos
+        binding.tvdni.text = usuario.dni
+        binding.tvplaca.text = usuario.placa
 
     }
+
+    private fun logout() {
+
+       val sharedPref = SharedPrefCons(requireActivity())
+        if(sharedPref.getSomeBooleanValue("mantener")){
+            sharedPref.remove("mantener")
+        }
+
+        val i = Intent(requireActivity(), LoginActivity::class.java)
+        startActivity(i)
+        requireActivity().finish()
+
+
+    }
+
+
+
+
+
+
 
 }
